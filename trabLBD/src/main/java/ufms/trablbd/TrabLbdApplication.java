04f3fbbe7b8df58236ec62f487fb1555e9c5077f
juliacamposn <neryjuliacampos@gmail.com>
2024-11-24 21:36:31 -0400

    feat: criando o projeto spring

---
+++ trabLBD/src/main/java/ufms/trablbd/TrabLbdApplication.java
@@ -0,0 +1,13 @@
+package ufms.trablbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabLbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabLbdApplication.class, args);
+    }
+
+}
